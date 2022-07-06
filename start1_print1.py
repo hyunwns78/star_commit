1c308cdc948dcb02019e3a610190dc3595cad06b
김현준 <hyunwns78@yonsei.ac.kr>
2022-07-06 09:17:32 +0900

    start3_print_V6.py

--- start1_print1.py
+++ start1_print1.py
@@ -1,12 +1,14 @@
-# *
-# * *
-# * * *
-# * * * *
 # * * * * *
+#   * * * *
+#     * * *
+#       * *
+#         *
 
-for i in range(1,6): #1부터 6-1까지
-    for j in range(i):
-        print('*',end="")
+for i in range(5,0,-1): #1range(A, B, C) A부터 C 숫자만큼의 간격으로 B-1 까지의 정수 범위를 반환합니다.
     for j in range(5-i):
         print(' ',end="")
+    for j in range(i):
+        print('*',end="")
+        
     print('')
+    
